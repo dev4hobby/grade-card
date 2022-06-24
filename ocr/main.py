0cb8871ee2801d21417b57a3a1111199330095b6
d3fau1t <aciddust20@gmail.com>
2022-06-24 14:03:49 +0900

    fix: new const for data dir

--- ocr/main.py
+++ ocr/main.py
@@ -2,14 +2,14 @@ import os
 from utils import get_files_from_dir, classify_by_extension
 from pdf_converter import pdf2jpg
 from img_processor import split_area
-from constants import DATA_DIR
+from constants import DATA_DIR, JPG_DIR
 
 if __name__ == "__main__":
     file_map_by_ext = classify_by_extension(get_files_from_dir(DATA_DIR))
 
     if "jpg" not in file_map_by_ext:
         if "pdf" in file_map_by_ext:
-            pdf2jpg(file_map_by_ext["pdf"], os.path.join(DATA_DIR, "jpg"))
+            pdf2jpg(file_map_by_ext["pdf"], JPG_DIR)
 
         file_map_by_ext = classify_by_extension(get_files_from_dir(DATA_DIR))
 
