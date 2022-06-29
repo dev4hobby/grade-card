f6d3ed334e660e34fdd5d18c0671b26bac92f744
d3fau1t <aciddust20@gmail.com>
2022-06-29 15:37:20 +0900

    feat: deprecate old api server

--- api-server/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import cv2
-import boto3
-import numpy as np
-import pytesseract
-from pdf2image import convert_from_bytes
--- api-server/utils/converter.py
+++
@@ -1,6 +0,0 @@
-from . import np, cv2, convert_from_bytes
-
-def convert_bytes_as_pages(bytes, dpi=500):
-    pages = convert_from_bytes(bytes, dpi)
-    pages = [cv2.cvtColor(np.array(page), cv2.COLOR_RGB2BGR) for page in pages]
-    return pages
