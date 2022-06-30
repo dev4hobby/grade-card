8c9bb6ac9954f8ea7d623494f3f461ba68e9a02a
d3fau1t <aciddust20@gmail.com>
2022-07-01 01:46:26 +0900

    fix: router path

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .oauth.kakao import kakao_login, kakao_callback, KakaoLogin
 
 urlpatterns = [
-    path("kakao/login", kakao_login, name="kakao_login"),
-    path('kakao/callback', kakao_callback, name='kakao_callback'),
-    path('kakao/login/finish', KakaoLogin.as_view(), name='kakao_login_finish'),
+    path("kakao/login/", kakao_login, name="kakao_login"),
+    path('kakao/callback/', kakao_callback, name='kakao_callback'),
+    path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_finish'),
 ]
