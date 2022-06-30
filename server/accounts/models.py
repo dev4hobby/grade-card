2f4603858ed77648195ab201d76c39761f322d5a
d3fau1t <aciddust20@gmail.com>
2022-07-01 00:26:08 +0900

    feat: add user model and user manager

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,81 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
+from .managers import UserManager
 
-    def _create_user(self, email, password, **extra_fields):
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
         if not email:
-            raise ValueError("The given email must be set")
-        
+            raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
-    
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
+            raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
 
-        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    """
-    Custom user model
-    """
-    email = models.EmailField(
-        verbose_name=_('email id'),
-        max_length=128,
-        unique=True,
-        db_index=True,
-        null=False,
-        help_text=_('Required. 128 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-    )
-    username = models.CharField(
-        max_length = 30,
-    )
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
-    )
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_('Designates whether this user should be treated as active. '
-                    'Unselect this instead of deleting accounts.'),
-    )
-    date_joined = models.DateTimeField(
-        _('date joined'),
-        default=timezone.now,
-        help_text=_('Designates when the user joined.'),
-    )
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     objects = UserManager()
 
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-        ordering = ('-date_joined',)
-
-    def __str__(self):
-        return self.username
-    
-    def get_short_name(self):
-        return self.email
+    spouse_name = models.CharField(blank=True, max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
