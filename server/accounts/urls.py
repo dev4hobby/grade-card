544c422f3728684c69b469e2df8ecd6df1e78aba
d3fau1t <aciddust20@gmail.com>
2022-07-01 03:13:44 +0900

    feat: add blacklist router

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .oauth.kakao import kakao_login, kakao_callback, KakaoLogin
+from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
+    path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("kakao/login/", kakao_login, name="kakao_login"),
     path('kakao/callback/', kakao_callback, name='kakao_callback'),
     path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_finish'),
