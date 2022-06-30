8d7403f83265610da3274f5dabcc40f411165177
d3fau1t <aciddust20@gmail.com>
2022-06-30 20:05:47 +0900

    fix: mysql host name

--- server/app/settings_local.py
+++ server/app/settings_local.py
@@ -6,7 +6,7 @@ DATABASES = {
         "NAME": "db",
         "USER": "username",
         "PASSWORD": "password",
-        "HOST": "mysql",
+        "HOST": "local_mysql",
         "PORT": "3306",
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'; SET foreign_key_checks = 0;",
