f0a6c0a71b5577fef0a77e197a6260ca07ce9e34
d3fau1t <aciddust20@gmail.com>
2022-07-01 00:27:22 +0900

    feat: add kakao router

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import getUserInfo, kakaoGetLogin
+from .oauth.kakao import kakao_login, kakao_callback, KakaoLogin
 
 urlpatterns = [
-    path("kakao", kakaoGetLogin),
-    path('kakao/callback', getUserInfo),
+    path("kakao/login", kakao_login, name="kakao_login"),
+    path('kakao/callback', kakao_callback, name='kakao_callback'),
+    path('kakao/login/finish', KakaoLogin.as_view(), name='kakao_login_finish'),
 ]
